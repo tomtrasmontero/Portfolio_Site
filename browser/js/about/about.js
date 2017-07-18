dc74c492a1ece67f61136c9d5d83a2a3f29083a5
tomtrasmontero <tomtrasmontero@gmail.com>
2017-07-17 22:18:18 -0400

    add new assets and tweak homepage layout

--- browser/js/about/about.js
+++ browser/js/about/about.js
@@ -3,13 +3,8 @@ app.config(function ($stateProvider) {
     // Register our *about* state.
     $stateProvider.state('about', {
         url: '/about',
-        controller: 'AboutController',
         templateUrl: 'js/about/about.html'
     });
 
 });
 
-app.controller('AboutController', function ($scope) {
-
-
-});
