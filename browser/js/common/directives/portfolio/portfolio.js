43b7b8902e7673d50d4de8cf12332c6d59cb232e
tomtrasmontero <tomtrasmontero@gmail.com>
2017-07-21 02:30:49 -0400

    added seperator and clean up code

---
+++ browser/js/common/directives/portfolio/portfolio.js
@@ -0,0 +1,7 @@
+app.config(function ($stateProvider) {
+
+    $stateProvider.state('portfolio', {
+        url: '/portfolio',
+        templateUrl: 'js/common/directives/portfolio/portfolio.html'
+    });
+});
